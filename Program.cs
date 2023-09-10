311ded6f1e96d3c643e808d75a85f08ae62209c7
julesrx <25800503+julesrx@users.noreply.github.com>
2023-09-10 18:48:43 +0200

    Use Lifetime ApplicationStarted

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
 using Dotnet.Web.Nuxt;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.GenerateSpaProxyRoutes();
-
 var app = builder.Build();
 
 app.MapGet("/hello", () => "Hello World!");
 
 app.UseStaticFiles();
 app.MapFallbackToFile("index.html");
+
+app.GenerateSpaProxyRoutes();
 
 app.Run();
--- ProxyGeneratorWorker.cs
+++ ProxyGeneratorWorker.cs
@@ -3,56 +3,31 @@ using System.Text.Json;
 
 namespace Dotnet.Web.Nuxt;
 
-public sealed class ProxyGeneratorWorker : BackgroundService
-{
-    private readonly IHostApplicationLifetime _lifetime;
-    private readonly IWebHostEnvironment _env;
-    private readonly IEnumerable<EndpointDataSource> _endpointSources;
-
-    public ProxyGeneratorWorker(IWebHostEnvironment env, IEnumerable<EndpointDataSource> endpointSources,
-        IHostApplicationLifetime lifetime)
-    {
-        _env = env;
-        _endpointSources = endpointSources;
-        _lifetime = lifetime;
-    }
-
-    protected override async Task ExecuteAsync(CancellationToken ct)
-    {
-        while (!_lifetime.ApplicationStarted.IsCancellationRequested)
-        {
-            await GenerateProxyRoutes();
-            await Task.Delay(1000, ct);
-        }
-    }
-
-    private async Task GenerateProxyRoutes()
-    {
-        var routes = _endpointSources
-            .SelectMany(s => s.Endpoints)
-            .Where(e => e is RouteEndpoint)
-            .Cast<RouteEndpoint>();
-
-        var paths = routes
-            .Select(r => r.RoutePattern.RawText)
-            .Where(t => !string.IsNullOrEmpty(t) && !t.Contains("nonfile"))
-            .Distinct()
-            .ToList();
-
-        var filename = Path.Combine(_env.ContentRootPath, "ClientApp/proxy-paths.json");
-        var json = JsonSerializer.Serialize(
-            paths, new JsonSerializerOptions(JsonSerializerOptions.Default) { WriteIndented = true }
-        );
-
-        await File.WriteAllTextAsync(filename, json);
-    }
-}
-
 public static class ProxyGenerator
 {
     [Conditional("DEBUG")]
-    public static void GenerateSpaProxyRoutes(this WebApplicationBuilder builder)
+    public static void GenerateSpaProxyRoutes(this WebApplication app)
     {
-        builder.Services.AddHostedService<ProxyGeneratorWorker>();
+        app.Lifetime.ApplicationStarted.Register(() =>
+        {
+            var sources = app.Services.GetRequiredService<IEnumerable<EndpointDataSource>>();
+
+            var routes = sources
+                .SelectMany(s => s.Endpoints)
+                .Where(e => e is RouteEndpoint)
+                .Cast<RouteEndpoint>();
+
+            var paths = routes
+                .Select(r => r.RoutePattern.RawText)
+                .Where(t => !string.IsNullOrEmpty(t) && !t.Contains("nonfile"))
+                .Distinct();
+
+            var filename = Path.Combine(app.Environment.ContentRootPath, "ClientApp/proxy-paths.json");
+            var json = JsonSerializer.Serialize(
+                paths, new JsonSerializerOptions(JsonSerializerOptions.Default) { WriteIndented = true }
+            );
+
+            File.WriteAllText(filename, json);
+        });
     }
 }
