239149fb4b0a2dc922ac4ebef8b32d6e9f5568ac
julesrx <25800503+julesrx@users.noreply.github.com>
2023-09-10 18:19:20 +0200

    Update endpoints

--- ClientApp/nuxt.config.ts
+++ ClientApp/nuxt.config.ts
@@ -1,6 +1,6 @@
 import paths from './proxy-paths.json'
 
-const serverUrl = 'https://localhost:7100'
+const serverUrl = 'http://localhost:7100'
 
 const proxys: { [p: string]: { target: string, changeOrigin: true, secure: false } } = {}
 for (const path of paths) {
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Dotnet.Web.Nuxt;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/data", () => "Hello World!");
+app.MapGet("/hello", () => "Hello World!");
 
 app.UseStaticFiles();
 app.MapFallbackToFile("index.html");
