1db9cd594aac6ff77226736d9655c1785b5c5016
julesrx <25800503+julesrx@users.noreply.github.com>
2023-09-13 23:13:52 +0200

    Add auth services

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme);
+
 var app = builder.Build();
 
-app.MapGet("/_/hello", () => "Hello World!");
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapGet("/_/hello", () => "Hello World!").RequireAuthorization();
 
 app.UseStaticFiles();
 app.MapFallbackToFile("index.html");
