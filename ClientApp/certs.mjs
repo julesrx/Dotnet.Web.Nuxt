7037488b9c980acd9d5042ee31faead7b9516f4c
julesrx <dev@julesrx.fr>
2023-11-29 15:16:49 +0100

    fix: generate certificate name to prevent dupes

--- ClientApp/certs.mjs
+++ ClientApp/certs.mjs
@@ -1,19 +1,21 @@
 import { join } from 'path';
-import { existsSync, copyFileSync } from 'fs';
+import { existsSync, copyFileSync, writeFileSync, readFileSync } from 'fs';
 import { spawnSync } from 'child_process';
 
 const baseFolder = process.env.APPDATA
     ? `${process.env.APPDATA}/ASP.NET/https`
     : `${process.env.HOME}/.aspnet/https`;
 
-const certificateName = 'dotnet-nuxt';
+const nameFilePath = '.cert.name';
+if (!existsSync(nameFilePath)) writeFileSync(nameFilePath, crypto.randomUUID());
+const certificateName = readFileSync(nameFilePath);
 
-const certFilePath = join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = join(baseFolder, `${certificateName}.key`);
+const certFilePath = join(baseFolder, `.${certificateName}.pem`);
+const keyFilePath = join(baseFolder, `.${certificateName}.key`);
 
 if (!existsSync(certFilePath) || !existsSync(keyFilePath)) {
     spawnSync('dotnet', ['dev-certs', 'https', '--trust'], {
-        stdio: 'inherit'
+        stdio: 'inherit',
     });
 
     const res = spawnSync(
@@ -25,7 +27,7 @@ if (!existsSync(certFilePath) || !existsSync(keyFilePath)) {
             certFilePath,
             '--format',
             'Pem',
-            '--no-password'
+            '--no-password',
         ],
         { stdio: 'inherit' }
     );
@@ -33,5 +35,5 @@ if (!existsSync(certFilePath) || !existsSync(keyFilePath)) {
     if (res.status !== 0) process.exit(res.status);
 }
 
-copyFileSync(certFilePath, './cert.pem');
-copyFileSync(keyFilePath, './cert.key');
+copyFileSync(certFilePath, './.cert.pem');
+copyFileSync(keyFilePath, './.cert.key');
